2ce96a311b08f0906a8239c64719cae4142cee5c
Ja1m3st <jaimesanchezguinea@gmail.com>
2026-05-13 13:29:36 +0200

    update: deploy version 1 of the app

--- p3/scripts/entrypoint.sh
+++ p3/scripts/entrypoint.sh
@@ -1,8 +1,24 @@
+echo "Creating cluster..."
 k3d cluster create mycluster -p "8888:8888@loadbalancer"
+
+echo "Creating namespaces..."
 kubectl create namespace argocd
 kubectl create namespace dev
-kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-sleep 10
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
 
+echo "Installing Argo CD..."
+kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+echo "Waiting for Argo CD to fully start (this may take a couple of minutes)..."
+kubectl wait --for=condition=Available deployment/argocd-server -n argocd --timeout=300s
+
+echo "Waiting for the password to be generated..."
+while ! kubectl get secret argocd-initial-admin-secret -n argocd &> /dev/null; do
+    sleep 2
+done
+
+echo "Argo CD is ready! Your administrator password is:"
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+echo ""
+
+echo "Starting Argo CD"
 kubectl port-forward svc/argocd-server -n argocd 8080:443
