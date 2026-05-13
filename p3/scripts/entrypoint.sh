51828a362e51638ba3c176ea725f717cd13979bd
Ja1m3st <jaimesanchezguinea@gmail.com>
2026-05-13 13:17:15 +0200

    update: deploy version 2 of the app

--- p3/scripts/entrypoint.sh
+++ p3/scripts/entrypoint.sh
@@ -2,6 +2,7 @@ k3d cluster create mycluster -p "8888:8888@loadbalancer"
 kubectl create namespace argocd
 kubectl create namespace dev
 kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sleep 10
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
-igHIgtXxEuEejetX
+
 kubectl port-forward svc/argocd-server -n argocd 8080:443
